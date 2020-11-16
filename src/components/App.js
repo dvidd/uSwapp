c77681300ee46e9c75b743b4c0ad6a1ad1d90b2e
David L Barrero <davidlopezbarrero@gmail.com>
2020-11-16 10:08:01 +0100

    Conection to wallet

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,69 @@
 import React, { Component } from "react";
+import Navbar from "./Navbar";
+import Main from "./Main";
+import Web3 from "web3";
 import "./App.css";
 
 class App extends Component {
+  async componentWillMount() {
+    await this.loadWeb3();
+    this.loadBlockchainData();
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert(
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
+      );
+    }
+  }
+  async loadBlockchainData() {
+    if (window.ethereum) {
+      const web3 = new Web3(window.web3.currentProvider);
+      // Load account
+      const accounts = await web3.eth.getAccounts();
+      this.setState({ account: accounts[0] });
+      // Network ID
+      const networkId = await web3.eth.net.getId();
+    }
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      buffer: null,
+      account: "",
+      dvideo: null,
+      videos: [],
+      loading: false,
+      currentHash: null,
+      currentTitle: null
+    };
+  }
+
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow"></nav>
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto"></div>
-            </main>
+        <Navbar account={this.state.account} />
+        {this.state.loading ? (
+          <div id="loader" className="text-center mt-5">
+            <p>Loading...</p>
           </div>
-        </div>
+        ) : (
+          <Main
+            videos={this.state.videos}
+            uploadVideo={this.uploadVideo}
+            captureFile={this.captureFile}
+            changeVideo={this.changeVideo}
+            currentHash={this.state.currentHash}
+            currentTitle={this.state.currentTitle}
+          />
+        )}
       </div>
     );
   }
