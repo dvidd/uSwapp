0df995de3ff8374e1a6f9885833dc2c92c328a3a
David L Barrero <davidlopezbarrero@gmail.com>
2020-11-17 21:48:09 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,12 +11,11 @@ class Navbar extends Component {
     return (
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">
-          🌍 &nbsp; <b>Uswapp</b>
+          <img src={require("../assets/Uswapp.png")} /> &nbsp; <b>Uswapp</b>
         </a>
         <button
           class="navbar-toggler"
           type="button"
-          data-toggle="collapse"
           data-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent"
           aria-expanded="false"
