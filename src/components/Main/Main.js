a22ea6e57604cf1b7a2fbbfd85a35208e9f0b648
David L Barrero <davidlopezbarrero@gmail.com>
2020-11-21 21:03:03 +0100

    Payable functioin and creation of swap

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,16 +2,26 @@ import React, { Component } from "react";
 
 import Info from "../Shared/Info";
 import Card from "../Card/Card";
+import List from "../SwapList/List";
 
 class Main extends Component {
   render() {
     return (
       <div className="container-fluid mt-5">
         <Info></Info>
-        <Card
-          createNewSwap={this.props.createNewSwap}
-          balance={this.props.balance}
-        ></Card>
+        <div className="row">
+          <div
+            id="content mt-3"
+            className="col-lg-12 ml-auto mr-auto"
+            style={{ maxWidth: "600px" }}
+          >
+            <Card
+              createNewSwap={this.props.createNewSwap}
+              balance={this.props.balance}
+            ></Card>
+            {/* <List></List> */}
+          </div>
+        </div>
       </div>
     );
   }
