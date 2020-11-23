70b3f5440b34b822779ac1cb8b7f8ef8297c450a
David L Barrero <davidlopezbarrero@gmail.com>
2020-11-23 19:55:04 +0100

    Creation card and search card combine

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,37 +1,84 @@
 import React, { Component } from "react";
 
 import Info from "../Shared/Info";
-import Card from "../Card/Card";
+import CreationCard from "../CreationCard/CreationCard";
 import Swap from "../Swap/Swap";
+import SwapSearch from "../SwapSearch/SwapSearch";
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      swapId: "2"
+      swapId: "",
+      create: true
     };
   }
+  changeCard() {
+    this.setState({ create: !this.state.create });
+  }
+
+  renderSwap() {
+    const swap = this.props.swap;
+    if (swap !== undefined) {
+      return (
+        <div>
+          <Swap swap={swap} />
+        </div>
+      );
+    }
+  }
+
+  renderCard() {
+    const create = this.state.create;
+    if (create === true) {
+      return (
+        <CreationCard
+          createNewSwap={this.props.createNewSwap}
+          balance={this.props.balance}
+        />
+      );
+    } else {
+      return (
+        <div>
+          {this.renderSwap()}
+          <SwapSearch getSwap={this.props.getSwap} />
+        </div>
+      );
+    }
+  }
+
+  returnCardChanger() {
+    if (this.state.create === true) {
+      return (
+        <h5 className="changeCardtext" onClick={() => this.changeCard()}>
+          <ion-icon name="search-outline"></ion-icon> Search for a Swap
+        </h5>
+      );
+    } else {
+      return (
+        <h5 className="changeCardtext" onClick={() => this.changeCard()}>
+          <ion-icon name="add-circle-outline"></ion-icon> Create a new Swap
+        </h5>
+      );
+    }
+  }
+
   render() {
     return (
       <div className="container-fluid mt-5">
-        <Info></Info>
-
+        <Info />
         <div className="row">
           <div
             id="content mt-3"
             className="col-lg-12 ml-auto mr-auto"
             style={{ maxWidth: "600px" }}
           >
-            {this.state.swapId == "" ? (
-              <Card
-                createNewSwap={this.props.createNewSwap}
-                balance={this.props.balance}
-              ></Card>
-            ) : (
-              <Swap></Swap>
-            )}
+            {/* Render Card of creation or search/swap */}
+            {this.renderCard()}
           </div>
         </div>
+        {/* element for changing between views  */}
+        {this.returnCardChanger()}
       </div>
     );
   }
