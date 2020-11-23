e26680d23421e8c8ced479a7dc3fa6902a652431
David L Barrero <davidlopezbarrero@gmail.com>
2020-11-23 02:21:06 +0100

    Summary information card

--- src/components/Shared/Info.js
+++ src/components/Shared/Info.js
@@ -1,12 +1,9 @@
 import React, { Component } from "react";
 
-import SmallButton from "../Shared/SmallButton";
-
 class Info extends Component {
   slides = [
-    "uSwapp is a protocol for exchanging product and services base in the Ethereum blockchain.",
-    "Add a layer of security on your crypto transactions",
-    "Add a new swapp and commit to proccess the transaction once both part are ready"
+    "uSwapp is a swaping protocol for base on ETH",
+    " Send and receive ether, where the transaction will be done once both parties agree "
   ];
   constructor(props) {
     super(props);
@@ -14,13 +11,6 @@ class Info extends Component {
       onSlide: 0,
       display: true
     };
-    setInterval(() => {
-      if (this.state.onSlide != this.slides.length) {
-        this.setState({ onSlide: this.state.onSlide + 1 });
-      } else {
-        this.setState({ onSlide: 0 });
-      }
-    }, 5000);
   }
 
   render() {
@@ -28,7 +18,11 @@ class Info extends Component {
       return (
         <div class="cardInfo">
           <div class="container cardInfoContainer">
-            <p>{this.slides[this.state.onSlide]}</p>
+            <p>
+              {this.slides[0]}
+              <br />
+              {this.slides[1]}
+            </p>
           </div>
         </div>
       );
