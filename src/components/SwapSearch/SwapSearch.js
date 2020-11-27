a8cd4f7fa2f9a779d86b9a3e068e81e9c63cdea7
David L Barrero <davidlopezbarrero@gmail.com>
2020-11-27 04:28:47 +0100

    Search Swap form validation

--- src/components/SwapSearch/SwapSearch.js
+++ src/components/SwapSearch/SwapSearch.js
@@ -8,18 +8,38 @@ class Search extends Component {
     super(props);
     this.state = {
       id: null,
-      loading: false
+      loading: false,
+      notValidId: true,
+      touch: false
     };
     this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange(evt) {
-    this.setState({ [evt.target.name]: evt.target.value });
+  errors() {
+    this.setState({ touch: true });
+    var id = this.state.id;
+    if (id === 0 || id < 0 || id === null) {
+      this.setState({ notValidId: true });
+    } else {
+      this.setState({ notValidId: false });
+    }
   }
-  async getSwap() {
-    this.setState({ loading: true });
-    await this.props.getSwap(this.state.id);
-    this.setState({ loading: false });
+
+  handleChange(evt) {
+    this.setState({ id: evt.target.value });
+    setTimeout(() => {
+      this.errors();
+    }, 1000);
+  }
+  getSwap() {
+    if (this.state.notValidId === false && this.state.touch === true) {
+      this.setState({ loading: true });
+      this.props.getSwap(this.state.id);
+      this.setState({ loading: false });
+    } else {
+      // Errors
+      this.errors();
+    }
   }
   render() {
     return (
@@ -33,9 +53,14 @@ class Search extends Component {
                 type="number"
                 name="id"
                 className="form-control form-control-lg"
-                placeholder="0"
+                placeholder="#9494"
                 required
               />
+              {this.state.notValidId && this.state.touch && (
+                <div className="invalid-feedback d-block">
+                  Please enter a valid id
+                </div>
+              )}
             </div>
 
             {this.state.loading ? (
