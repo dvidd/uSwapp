3a2db2a25c2147c0ad1d1029d71dcbf0195bfef3
David L Barrero <davidlopezbarrero@gmail.com>
2021-01-17 20:01:04 +0100

    Rever ruting to last functional version

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,14 +1,72 @@
+
 import React, { Component } from "react";
 
 import CreationCard from "../CreationCard/CreationCard";
+import Swap from "../Swap/Swap";
+import SwapSearch from "../SwapSearch/SwapSearch";
 import Info from "../../components/Shared/Info";
 
 class Main extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      create: true // for more quick develop in the interface (( take this offf ))
+    };
+  }
+  changeCard() {
+    this.setState({ create: !this.state.create });
   }
 
+  renderSwap() {
+    const swap = this.props.swap;
+    if (swap !== undefined && swap !== null) {
+      return (
+        <div>
+          <Swap
+            id={this.props.id}
+            account={this.props.account}
+            swap={swap}
+            validSwap={this.props.validSwap}
+          />
+        </div>
+      );
+    }
+  }
+
+  renderCard() {
+    const create = this.state.create;
+    if (create) {
+      return (
+        <CreationCard
+          createNewSwap={this.props.createNewSwap}
+          balance={this.props.balance}
+        />
+      );
+    } else {
+      return (
+        <div>
+          {this.renderSwap()}
+          <SwapSearch getSwap={this.props.getSwap} />
+        </div>
+      );
+    }
+  }
+
+  returnCardChanger() {
+    if (this.state.create) {
+      return (
+        <h5 className="changeCardtext" onClick={() => this.changeCard()}>
+          <ion-icon name="search-outline"></ion-icon> Search for a Swap
+        </h5>
+      );
+    } else {
+      return (
+        <h5 className="changeCardtext" onClick={() => this.changeCard()}>
+          <ion-icon name="add-circle-outline"></ion-icon> Create a new Swap
+        </h5>
+      );
+    }
+  }
   returnInfo() {
     if (this.props.noWallet) {
       return <Info />;
@@ -26,13 +84,11 @@ class Main extends Component {
             style={{ maxWidth: "600px" }}
           >
             {/* Render Card of creation or search/swap */}
-            <CreationCard
-              createNewSwap={this.props.createNewSwap}
-              balance={this.props.balance}
-            />
+            {this.renderCard()}
           </div>
         </div>
         {/* element for changing between views  */}
+        {this.returnCardChanger()}
         <span>{this.props.latestID}</span>
       </div>
     );
